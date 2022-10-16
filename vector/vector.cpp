1ff4d1265bab741fa3fda7c3f3a531997b38af4d
Ahmed Maher <65620946+Ahmed336-cell@users.noreply.github.com>
2022-10-16 23:22:41 +0200

    Create vector.cpp

---
+++ vector/vector.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+  
+using namespace std;
+  
+int main()
+{
+    vector<int> vect; // empty vector
+    vector<int> v(5); // new vector of size 5
+    vector<bool> v2(5, true); // new vector of size 5 with all values initialized to true
+
+    // push_back
+    vect.push_back(2); // insert 2 at end of vector
+    vect.push_back(5);
+    vect.push_back(8);
+    vect.push_back(9);
+
+    auto it = vect.begin(); // get iterator to beginning
+    auto it2 = vect.end(); // get iterator to end (theoretically, the element after the last element)
+
+    // size
+    cout << vect.size() << endl;
+
+    // is_empty
+    cout << vect.empty() << endl;
+
+    // reference operator
+    cout << vect[0] << endl;
+    cout << vect[1] << endl;
+    cout << vect[2] << endl;
+
+    // front and back
+    cout << vect.front() << endl;
+    cout << vect.back() << endl;
+
+    // erase
+    vect.erase(vect.begin() + 4); //delete 4th element
+
+    // sort and reverse
+    sort(vect.begin(), vect.end()); //sort vector
+    reverse(vect.begin(), vect.end()); // reverse the vector
+
+    // pop_back
+    vect.pop_back(); // delete last element
+
+    // looping over vectors
+    cout << "---------------" << endl;
+    for (int n : vect) {
+        cout << n << endl;
+    }
+
+    cout << "---------------" << endl;
+    for (auto it = vect.begin(); it != vect.end(); it++) {
+        cout << *it << endl;
+    }
+    cout << "---------------" << endl;
+
+    // clear
+    vect.clear();
+}
